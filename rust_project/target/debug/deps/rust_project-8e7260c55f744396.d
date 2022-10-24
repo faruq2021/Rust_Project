d86d013228c6fafa8e76605fb23278b0efe85196
Dauda <umardauda@rocketmail.com>
2022-10-24 17:36:15 +0100

    New stuff about rust

---
+++ rust_project/target/debug/deps/rust_project-8e7260c55f744396.d
@@ -0,0 +1,5 @@
+c:\Users\HP\Rust_Project\rust_project\target\debug\deps\rust_project-8e7260c55f744396.rmeta: src\main.rs
+
+c:\Users\HP\Rust_Project\rust_project\target\debug\deps\rust_project-8e7260c55f744396.d: src\main.rs
+
+src\main.rs:
---
+++ rust_project/target/debug/deps/rust_project-d0ac28862e8c3424.d
@@ -0,0 +1,5 @@
+c:\Users\HP\Rust_Project\rust_project\target\debug\deps\rust_project-d0ac28862e8c3424.rmeta: src\main.rs
+
+c:\Users\HP\Rust_Project\rust_project\target\debug\deps\rust_project-d0ac28862e8c3424.d: src\main.rs
+
+src\main.rs:
